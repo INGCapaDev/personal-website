a8ff1517eabbbe6bfb783977449ca23d3230e053
Alvaro Capaceta <ingcapadev@gmail.com>
2023-05-21 23:46:55 -0700

    Fix light theme

--- theme.js
+++ theme.js
@@ -1,9 +1,19 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   let btn = document.querySelector('.toggle');
   let icon = document.querySelector('.toggle i');
-  let body = document.querySelector('.hero.header');
+  let hero = document.querySelector('.hero-header');
+  let html = document.querySelector('html');
 
   btn.onclick = () => {
-    body.classList.toggle('light-theme');
+    hero.classList.toggle('light-theme');
+    html.classList.toggle('light-theme');
+    if (hero.classList.contains('light-theme')) {
+      icon.classList.add('fa-moon');
+      icon.classList.remove('fa-sun');
+      return;
+    }
+    icon.classList.add('fa-sun');
+    icon.classList.remove('fa-moon');
   };
+  console.log('hola');
 });
